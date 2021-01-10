7dae6453fa3df2e1da873ea7017a8e8f9d2de356
caiorodrigues9 <caiorodrigues9@gmail.com>
2021-01-10 08:13:28 -0300

    Design pattern
    Començando o Curso de Design pattern

---
+++ design-pattern/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a308ec3b8993f3b1018f4328f7834a8c02041152',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a308ec3b8993f3b1018f4328f7834a8c02041152',
+    ),
+  ),
+);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a308ec3b8993f3b1018f4328f7834a8c02041152',
+    'name' => 'cviniciussdias/alura-solid',
+  ),
+  'versions' => 
+  array (
+    'cviniciussdias/alura-solid' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a308ec3b8993f3b1018f4328f7834a8c02041152',
+    ),
+  ),
+);
