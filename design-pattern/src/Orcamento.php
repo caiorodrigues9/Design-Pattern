8d6ec023e3b91059f9c33d0f9dfbbb7c75bd52de
caiorodrigues9 <caiorodrigues9@gmail.com>
2021-01-10 11:18:44 -0300

    'Teste pelo Visual Studio Code'

--- design-pattern/src/Orcamento.php
+++ design-pattern/src/Orcamento.php
@@ -6,4 +6,27 @@ class Orcamento
 {
     public int $quantidadeItens;
     public float $valor;
+    public string $estadoAtual;
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->calculaDescontoExtra();
+    }
+
+    public function calculaDescontoExtra(): float
+    {
+        if($this->estadoAtual == "EM APROVACAO"){
+            return $this->valor * 0.5;
+        }
+        
+        if($this->estadoAtual == "APROVADO"){
+            return $this->valor * 0.2;
+        }
+
+        throw new \DomainException(
+            message:'Orçamentos aprovados e reprovados não podem receber desconto extras'
+        );
+
+
+    }
 }
